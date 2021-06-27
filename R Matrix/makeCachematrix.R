94682e3b0430a69c93ed0c2973b7025a7c7daa7e
Tumelo Matji <86361740+TumeloMatji7@users.noreply.github.com>
2021-06-27 13:06:30 +0200

    Update makeCachematrix.R

--- R Matrix/makeCachematrix.R
+++ R Matrix/makeCachematrix.R
@@ -1,15 +1,24 @@
+
+## this is my matrix function
 makeCachematrix <- function(x = matrix()) {
+  
+  ## assignment of NULL TO inv
   inv <-  NULL
+  
+  ## created a  set function
   set <- function(y) {
       x <<- y
       inv <<- NULL
   }
+  ## get function
   get <- function() {x}
   setInverse <- function(inverse) {inv <<- inverse }
+  
+  ## get inverse using inv
   getInverse <- function() {inv}
   list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
-
+## Function called cachesolve
 cachesolve <- function(x, ...) {
   inv <- x$getInverse()
   
